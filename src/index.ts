253ce06b35634054053f719ba28bf73b0b593c25
Jamie Kyle <me@thejameskyle.com>
2020-12-03 13:35:08 -0800

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 export type { default as Bytes } from "./Bytes"
 export type { default as Context } from "./Context"
-export type { default as FileState } from "./FileState"
 export type { default as Diff } from "./Diff"
 export type { default as File } from "./File"
 export type { default as Files } from "./Files"
-export type { DiffThresholds, GitDiffStats, Messages } from "./types"
+export type { default as FileState } from "./FileState"
+export type { default as Line } from "./Line"
+export type {
+	StructuredDiffBaseChange,
+	StructuredDiffAddChange,
+	StructuredDiffDelChange,
+	StructuredDiffNormalChange,
+	StructuredDiffChange,
+	StructuredDiffChunk,
+	StructuredDiff,
+	DiffThresholds,
+	GitDiffStats,
+	Messages,
+} from "./types"
 
 export { default as Rule } from "./Rule"
 export { default as run } from "./run"
