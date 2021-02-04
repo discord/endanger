90489a661acbc0e4a6dfe5397848a3c4cdd0f5ba
Jamie Kyle <me@thejameskyle.com>
2021-02-04 13:12:29 -0800

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 export type { default as Bytes } from "./Bytes"
 export type { default as Context } from "./Context"
 export type { default as Diff } from "./Diff"
+export type { default as DiffLine } from "./DiffLine"
 export type { default as File } from "./File"
 export type { default as Files } from "./Files"
 export type { default as FileState } from "./FileState"
 export type { default as Line } from "./Line"
 export type {
+	StructuredDiffChangeType,
 	StructuredDiffBaseChange,
 	StructuredDiffAddChange,
 	StructuredDiffDelChange,
