10110d8bace50a59bf52a76e68a82b21cba3c490
Athena Yao <athena@discordapp.com>
2022-01-03 14:28:10 -0800

    Fix files.matches not working when it's a directory match

--- src/utils/matches.ts
+++ src/utils/matches.ts
@@ -1,7 +1,9 @@
+import path from "path"
 import micromatch from "micromatch"
 
 export default function matches(paths: string[], patterns: string[]): string[] {
+	const isBasename = patterns.every((pattern) => path.basename(pattern) === pattern)
 	return micromatch(paths, patterns, {
-		basename: true,
+		basename: isBasename,
 	})
 }
