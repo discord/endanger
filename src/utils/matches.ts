b8925ca932a9eefdda10007e0c0317d5fbc8934b
Athena Yao <athena@discordapp.com>
2022-01-03 15:05:00 -0800

    add comment

--- src/utils/matches.ts
+++ src/utils/matches.ts
@@ -2,6 +2,11 @@ import path from "path"
 import micromatch from "micromatch"
 
 export default function matches(paths: string[], patterns: string[]): string[] {
+	// https://github.com/micromatch/micromatch/#options implies that `basename` only applies to patterns without slashes
+	// However, that doesn't seem to be the case in practice; this means that files.matches does not match when
+	//     path=some_directory/File.tsx
+	//     pattern=some_directory/**/*
+	// We ignore patterns with subdirectories for now, pending resolution of https://github.com/micromatch/picomatch/issues/89
 	const isBasename = patterns.every((pattern) => path.basename(pattern) === pattern)
 	return micromatch(paths, patterns, {
 		basename: isBasename,
