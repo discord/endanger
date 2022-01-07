9d39f72acde0157087c9b4e6787eed1669f7b0b9
Athena Yao <athena@discordapp.com>
2022-01-07 14:32:30 -0800

    Fix files.matches not working when it's a directory match (#15)
    * Fix files.matches not working when it's a directory match
    
    * add comment
    
    * Warn when we have mixed patterns, with some basename and some nested
    
    * basename true only if everything is basename and no nested
    
    * Use find for clarity

--- src/utils/matches.ts
+++ src/utils/matches.ts
@@ -1,7 +1,22 @@
+import path from "path"
 import micromatch from "micromatch"
 
 export default function matches(paths: string[], patterns: string[]): string[] {
+	// https://github.com/micromatch/micromatch/#options implies that `basename` only applies to patterns without slashes
+	// However, that doesn't seem to be the case in practice; this means that files.matches does not match when
+	//     path=some_directory/File.tsx
+	//     pattern=some_directory/**/*
+	// We ignore patterns with subdirectories for now, pending resolution of https://github.com/micromatch/picomatch/issues/89
+
+	let hasBasename = patterns.find((pattern) => path.basename(pattern) === pattern) != null
+	let hasNested = patterns.find((pattern) => path.basename(pattern) !== pattern) != null
+
+	if (hasBasename && hasNested) {
+		console.warn(
+			"Warning: Potentially misconfigured rule. `matches` can only handle patterns that are all a bare basename (`*.js`) or all nested (`x/**/*.js`).",
+		)
+	}
 	return micromatch(paths, patterns, {
-		basename: true,
+		basename: hasBasename && !hasNested,
 	})
 }
